2abb0dd8a9df9431e11bd35a7b4742b84f4eaca6
Tharusha Jayadeera <tharusha.pasindu@ymail.com>
2017-04-11 23:31:03 +0530

    The first and the main page of the website

---
+++ main.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+<form action="2.php" method="POST">
+
+<img src="logo.gif" width=180px>
+<img src="lms.jpg">
+
+<table align="right" bgcolor="AAFFFF">
+<caption><h3>You are not logged in...</h3></caption>
+<tr>
+<td>User Name:</td>
+<td><input type="text" name="username" /></td>
+</tr>
+<td>Password:</td>
+<td><input type="password" name="password" /></td>
+</tr>
+<tr>
+<td colspan="2" align="center"><input type="submit" value="Log In" /></td>
+</tr>
+</table>
+
+</form>
+
+</body>
+</html>
